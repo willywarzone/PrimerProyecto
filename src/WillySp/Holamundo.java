f97c7e0509fd71b1e787f5d70ceccf771b01f7bb
willywarzone <willy.y.serranopilozo@gmail.com>
2022-10-07 18:57:19 +0200

    Verision1

--- src/WillySp/Holamundo.java
+++ src/WillySp/Holamundo.java
@@ -11,7 +11,7 @@ package WillySp;
 public class Holamundo { 
      public static void main(String[] args) {
         // TODO code application logic here
-        
+        System.out.println("¡hola, Mundo!");
         }
 
 }
